ed168b2ef667de7e7e72d0eb37096fd73542ea28
Любовь Хуснуллина <lyuba140296@gmail.com>
2022-05-11 18:48:48 +0500

    Выделите ячейки по диагонали

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,9 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  for (let row of table.rows) {
+    for (let cell of row.cells) {
+      if (row.rowIndex === cell.cellIndex) {
+        cell.style.backgroundColor = 'red';
+      }
+    }
+  }
 }
